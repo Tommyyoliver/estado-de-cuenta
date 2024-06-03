f84238b16d307d483d6a88cb32a44153298877e6
Tommyyoliver <tommyolivr@gmail.com>
2024-06-03 16:52:28 -0300

    datos desde objeto

--- src/app/components/estado-de-cuenta/estado-de-cuenta.component.ts
+++ src/app/components/estado-de-cuenta/estado-de-cuenta.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { IEstadoDeCuenta } from './model/estado-de-cuenta'
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-estado-de-cuenta',
   standalone: true,
-  imports: [],
+  imports: [DecimalPipe, DatePipe],
   templateUrl: './estado-de-cuenta.component.html',
   styleUrl: './estado-de-cuenta.component.scss'
 })
 export class EstadoDeCuentaComponent {
+
+  datos: IEstadoDeCuenta = {
+    cliente: 200001901,
+    areaCtrlCreditos: "AG16",
+    areaControlCredAQUACHILE: "USD",
+    limiteCredito: 3000000,
+    cuentaCredito: 200001901,
+    gradoAgotamiento: 110.37,
+    feHorizonteCredito: new Date(2024, 2, 7),
+    creditos: 527123.3860,
+    comprometEsp: 121319.6200,
+    valComercial: 2905221.9583,
+    compromTotal: 3311025.7343,
+    creditosAseg: 0.0000,
+  }
 
 }
---
+++ src/app/components/estado-de-cuenta/model/estado-de-cuenta.ts
@@ -0,0 +1,14 @@
+export interface IEstadoDeCuenta {
+    cliente: number,
+    areaCtrlCreditos: string,
+    areaControlCredAQUACHILE: string,
+    limiteCredito: number,
+    cuentaCredito: number,
+    gradoAgotamiento: number,
+    feHorizonteCredito: Date,
+    creditos: number,
+    comprometEsp: number,
+    valComercial: number,
+    compromTotal: number,
+    creditosAseg: number,
+}
